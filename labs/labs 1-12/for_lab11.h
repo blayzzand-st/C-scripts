899ea4f84143c5930ef743aad3994fcb31ea76d0
Blayzzand <112903020+blayzzand-st@users.noreply.github.com>
2022-12-03 23:35:55 +0300

    Delete for_lab11.h

--- labs/labs 1-12/for_lab11.h
+++
@@ -1,8 +0,0 @@
-#ifndef LABS_FOR_LAB11_H
-#define LABS_FOR_LAB11_H
-
-double perimeter(double side1, double side2);
-double square(double side1_x, double side1_y, double side2_x, double side2_y);
-
-#endif
-
